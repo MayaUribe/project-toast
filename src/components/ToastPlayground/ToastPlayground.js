988ed3c65dde711af15039fad3f3a05eb08cf871
Maya Uribe <mariale.uribe@gmail.com>
2025-02-05 17:07:31 +0100

    editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '../Button';
+import Toast from '../Toast';
 
 import styles from './ToastPlayground.module.css';
 
@@ -9,11 +10,12 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
   const [message, setMessage] = React.useState('');
+  const [isToastVisible, setIsToastVisible] = React.useState(false);
 
   const handlePopToast = (event) => {
     event.preventDefault();
 
-    console.log(message, variant);
+    setIsToastVisible(true);
   };
 
   return (
@@ -22,6 +24,13 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      <Toast
+        message={message}
+        variant={variant}
+        isVisible={isToastVisible}
+        onClose={() => setIsToastVisible(false)}
+      />
 
       <div className={styles.controlsWrapper}>
         <form onSubmit={handlePopToast}>
@@ -38,6 +47,7 @@ function ToastPlayground() {
                 id="message"
                 className={styles.messageInput}
                 value={message}
+                required
                 onChange={(event) => setMessage(event.target.value)}
               />
             </div>
@@ -69,7 +79,7 @@ function ToastPlayground() {
           <div className={styles.row}>
             <div className={styles.label} />
             <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-              <Button>Pop Toast!</Button>
+              <Button type="submit">Pop Toast!</Button>
             </div>
           </div>
         </form>
