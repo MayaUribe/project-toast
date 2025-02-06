d4cf74dbae5b53a7a21dfb3037423dc0c6418283
Maya Uribe <mariale.uribe@gmail.com>
2025-02-06 11:34:47 +0100

    create custom hook

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
+import useKeydown from '../hooks/useKeydown';
+
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
-  React.useEffect(() => {
-    const dismissAllToasts = (e) => {
-      if (e.key === 'Escape') {
-        setToasts([]);
-      }
-    };
-
-    window.addEventListener('keydown', dismissAllToasts);
-
-    return () => {
-      window.removeEventListener('keydown', dismissAllToasts);
-    };
-  }, []);
+  const handleEscape = React.useCallback(() => setToasts([]), []);
+  useKeydown('Escape', handleEscape);
 
   const dismissToast = (id) => {
     setToasts(toasts.filter((toast) => toast.id !== id));
